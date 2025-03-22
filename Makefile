c9c7fe614987f0c20bd009e406a1eeef9aaa693b
Raphael Silva <eng.raphaelsn@gmail.com>
2025-03-21 21:43:22 -0300

    save: going to traveling

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+PYTHON=(uv run python)
+
 
 help: ## this help
 	@echo "Usage: make [target]"
@@ -6,10 +8,46 @@ help: ## this help
 	@awk 'BEGIN {FS = ":.*?##"} /^[a-zA-Z_-]+:.*?## / { printf "\t\033[36m%-20s\033[0m\t%s\n", $$1, $$2 }' $(MAKEFILE_LIST) | sort
 
 install: ## install project dependencies
-	poetry install
+	uv install
 
 generate-features: ## generate features
 	@echo "Generating features for ${PWD}"
 	@read -p "Enter feature name: " feature_name; \
 	uv run generate/feature/folders.py -b ${PWD} -f $$feature_name
 	uv run generate/feature/tasks.py -b ${PWD} -f $$feature_name
+
+coverage: ## This will run the tests and generate a coverage report
+	$(PYTHON) -m pytest -v --cov-report term-missing --cov=src/ --cov-report=xml --disable-warnings
+
+coverage-unitary: ## This will run the tests for unitary tests and generate a coverage report
+	$(PYTHON) -m pytest tests/unitary -v \
+		--cov-report term-missing \
+		--cov=src/ \
+		--cov-report=xml \
+		--disable-warnings \
+		--cov-branch
+
+test: ## This will run the tests
+	$(PYTHON) -m pytest
+
+lint: ## This will run the linter
+	$(PYTHON) -m bandit src/ -r
+	$(PYTHON) -m black src/
+	$(PYTHON) -m black tests/
+
+install-requirements: ## This will install the requirements
+	uv sync
+
+install-requirements-ci: ## This will install the requirements for CI
+	pip install poetry
+	uv install --no-root 
+
+
+scan-complexity: ## This will run the complexity scanner
+	radon cc source/ -a
+
+clean: ## Remove cache files
+	@find . -name "*.pyc" | xargs rm -rf
+	@find . -name "*.pyo" | xargs rm -rf
+	@find . -name "__pycache__" -type d | xargs rm -rf
+	@find . -name ".pytest_cache" -type d | xargs rm -rf
--- tests/unitary/feature/backup_photos_from/infrastructure/drivers/rabbitmq/test_adapter.py
+++ tests/unitary/feature/backup_photos_from/infrastructure/drivers/rabbitmq/test_adapter.py
@@ -1,4 +1,5 @@
 import os
+import re
 from unittest.mock import AsyncMock, patch
 
 import pytest
@@ -46,31 +47,35 @@ async def test_receive_message(mock_read_message):
 @pytest.mark.asyncio
 @patch.object(RabbitMQTopicClient, "get_client", new_callable=AsyncMock)
 async def test_create_message(mock_get_client):
-    os.environ["RABBITMQ_TOPIC_NAME"] = "test_topic"
-    os.environ["RABBITMQ_ROUTING_KEY"] = "test_routing_key"
-
+    topic_name = "test_topic"
+    routing_key = "test_routing_key"
+    os.environ["RABBITMQ_TOPIC_NAME"] = topic_name
+    os.environ["RABBITMQ_ROUTING_KEY"] = routing_key
     mock_topic = AsyncMock()
     mock_topic.publish = AsyncMock()
     mock_topic.publish.return_value = None
-
     mock_channel = AsyncMock()
-    mock_channel.exchange_declare = AsyncMock()
-    mock_channel.exchange_declare.return_value = None
-
+    mock_channel.declare_exchange = AsyncMock()
+    mock_channel.declare_exchange.return_value = mock_topic
+    mock_channel.publisher_confirms = True
     mock_client = AsyncMock()
+    mock_client.is_closed = False
     mock_client.channel = AsyncMock()
     mock_client.channel.return_value = mock_channel
-
     mock_get_client.return_value = mock_client
 
 
-    client = RabbitMQTopicClient()
-    topic_name = "test_topic"
-    routing_key = "test_routing_key"
+    client = RabbitMQTopicClient()    
     message = "test message"
-    await client.create_message(topic_name, routing_key, message)
+    response = await client.create_message(topic_name, routing_key, message)
+    assert response.get("confirmation")
+    assert isinstance(response.get("message_id"), str)
+    assert re.match(
+        r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
+        response.get("message_id"),
+    ), "Message ID is not a valid UUID"
     mock_get_client.assert_called_once()
     mock_client.channel.assert_called_once()
-    mock_channel.exchange_declare.assert_awaited_once_with(topic_name, 'topic')
-    mock_channel.publish.assert_awaited_once()
-    mock_channel.close.assert_awaited_once()
+    mock_channel.declare_exchange.assert_awaited_once_with(topic_name, 'topic')
+    mock_topic.publish.assert_awaited_once()
+    mock_client.close.assert_awaited_once()
