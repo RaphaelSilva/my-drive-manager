69d8035490bf97598aa7eaf83cc2021357950dbe
Raphael Silva Do Nascimento <raphael.nascimento@grupoboticario.com.br>
2025-08-07 09:26:56 -0300

    refactor: Remove unused pytest import and async marker from test_send_message

--- tests/unitary/feature/backup_photos_from/infrastructure/drivers/mediainfo/test_adapter.py
+++ tests/unitary/feature/backup_photos_from/infrastructure/drivers/mediainfo/test_adapter.py
@@ -1,13 +1,10 @@
 from datetime import datetime
 from unittest.mock import patch, Mock
 
-import pytest
-
 from pymediainfo import MediaInfo
 from src.feature.backup_files_from.infrastructure.drivers.mediainfo.adapter import MediaInfoAdapter
 
 
-@pytest.mark.asyncio
 @patch.object(MediaInfo, "parse", new_callable=Mock)
 def test_send_message(mock_create_message):
     mock_create_message.return_value = Mock(
